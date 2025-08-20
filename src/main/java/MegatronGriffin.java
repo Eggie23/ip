ccc1e84953a10fbf1731aa2ae7eddffa630850d2
Glendon Goh <167951234+Eggie23@users.noreply.github.com>
2025-08-21 01:26:28 +0800

    Add, List

--- src/main/java/MegatronGriffin.java
+++ src/main/java/MegatronGriffin.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MegatronGriffin {
@@ -6,12 +8,22 @@ public class MegatronGriffin {
         String end = "Uh… see ya. Try not to forget I exist, I guess.";
         System.out.println(start);
 
+        List<String> list = new ArrayList<>();
+
         Scanner myObj = new Scanner(System.in);
 
         String text = myObj.nextLine();
 
         while (!text.equals("bye")) {
-            System.out.println(text);
+            if (text.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    String str = String.format("%s. %s \n", i+1, list.get(i));
+                    System.out.println(str);
+                }
+            } else {
+                System.out.println("added: " + text);
+                list.add(text);
+            }
             text = myObj.nextLine();
         }
 
